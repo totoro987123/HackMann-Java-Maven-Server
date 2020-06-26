461c3a0546ae86d10f9a4d775c5dc9683d6ec74e
Lawson Wright <lawsonwright@icloud.com>
2020-06-26 18:49:49 -0400

    master

--- src/main/java/com/hackmann/player/Player.java
+++ src/main/java/com/hackmann/player/Player.java
@@ -1,29 +1,33 @@
+package com.hackmann.player;
+
+import com.hackmann.server.*;
+
 public class Player {
 
     private int score = 0;
     private String username = "";
     private Connection connection = null; //server id and socket
 
-    public Player() {}
-
-    public Player(String username, Connection connection) { //when a new player enters
-        this.username = username;
+    public Player(Connection connection) { //when a new player enters
         this.connection = connection;
     }
 
-    public String setScore(int score) {
-        this.score = score;
+    public void changeScore(int amount) {
+        this.score += score;
     }
+
     public String getUsername() {
         return this.username;
     }
+
+    public void setUsername(String name){
+        this.username = name;
+    }
+
     public int getScore() {
         return this.score;
     }
     public void disconnect() {
-        //disconnect player, put I think Lawson already did this somewhere else
-        connection.close();
+        this.connection.close();
     }
-
-    public static void main(String[] args) {}
 }
